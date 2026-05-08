225c668f343475ce40b0322b1806c45bd03a23a1
bob231323 <162380035+bob231323@users.noreply.github.com>
2026-05-08 20:10:53 +0300

    Add files via upload

---
+++ myfacttable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Fact_VendorPerformance (
+    vendor_key        INT,
+    date_key          INT,
+    total_trips       INT,
+    avg_trip_duration_sec FLOAT,
+    avg_passenger_count   FLOAT
+);
